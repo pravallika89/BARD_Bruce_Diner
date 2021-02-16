6624dc9efeaec540050c96bcff50f9076ed6acaf
pravallika <pravallikaammineni@github.com>
2021-02-15 23:24:44 -0800

    first commit

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+
+// const firstName=document.getElementById("firstName");
+// const lastName=document.getElementById("lastName");
+// const phone=document.getElementById("phone");
+
+
+// const button = document.getElementById('bn');
+// button.addEventListener('click',submitInfo());
+
+// button.addEventListener("click",function(){
+//   console.log(firstName.value);
+  
+//   })
+
+
